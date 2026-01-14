ceeb11e6f09ebf6ffe0e8baa7c3715d428db7d3b
preranalimaye423-lgtm <preranalimaye423@gmail.com>
2026-01-14 11:34:18 +0530

    Update hospitalB_mysqlToLanding.py

--- data/INGESTION/hospitalB_mysqlToLanding.py
+++ data/INGESTION/hospitalB_mysqlToLanding.py
@@ -26,10 +26,10 @@ BQ_TEMP_PATH = f"{GCS_BUCKET}/temp/"
 
 # MySQL Configuration
 MYSQL_CONFIG = {
-    "url": "jdbc:mysql://34.59.188.6:3306/hospital_b_db?useSSL=false&allowPublicKeyRetrieval=true",
+    "url": "jdbc:mysql://34.66.84.254:3306/hospital_b_db?useSSL=false&allowPublicKeyRetrieval=true",
     "driver": "com.mysql.cj.jdbc.Driver",
-    "user": "myuser",
-    "password": "mypass"
+    "user": "prerana",
+    "password": "Pre@1100"
 }
 
 ##------------------------------------------------------------------------------------------------------------------##
